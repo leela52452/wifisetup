75fa13cda92f73981144a0598bb73913490985ce
David Lamparter <equinox@diac24.net>
2015-07-31 22:31:27 +0200

    intermediate backup

--- android/src/nl/nikhef/eduroam/WiFiEduroam.java
+++ android/src/nl/nikhef/eduroam/WiFiEduroam.java
@@ -85,9 +85,10 @@ import android.net.wifi.WifiEnterpriseConfig.Eap;
 
 public class WiFiEduroam extends Activity {
 	// FIXME This should be a configuration setting somehow
-	private static final String CONF_HTTP_URL = "https://mobi.nikhef.nl/provision/";
+	private static final String CONF_HTTP_URL = "https://aurora.nox.tf/spacenet/android.json";
 	
 	private static final String INT_EAP = "eap";
+	private static final String INT_PHASE2 = "phase2";
 	private static final String INT_ENGINE = "engine";
 	private static final String INT_ENGINE_ID = "engine_id";
 	private static final String INT_CLIENT_CERT = "client_cert";
@@ -95,6 +96,8 @@ public class WiFiEduroam extends Activity {
 	private static final String INT_PRIVATE_KEY = "private_key";
 	private static final String INT_PRIVATE_KEY_ID = "key_id";
 	private static final String INT_SUBJECT_MATCH = "subject_match";
+	private static final String INT_PASSWORD = "password";
+	private static final String INT_IDENTITY = "identity";
 	private static final String INT_ANONYMOUS_IDENTITY = "anonymous_identity";
 	private static final String INT_ENTERPRISEFIELD_NAME = "android.net.wifi.WifiConfiguration$EnterpriseField";
 	
@@ -105,15 +108,10 @@ public class WiFiEduroam extends Activity {
 	private static final String INT_PRIVATE_KEY_ID_PREFIX = "USRPKEY_";
 	private static final String INT_CLIENT_CERT_PREFIX = INT_KEYSTORE_URI + "USRCERT_";
 	
-	private static final String INT_CLIENT_CERT_NAME = "client certificate";
-
 	protected static final int SHOW_PREFERENCES = 0;
-	protected static AlertDialog alertDialog;
-	private CSR csr;
     private Handler mHandler = new Handler();
 	private EditText username;
 	private EditText password;
-	private String certificate;
 	private String ca;
 	private String ca_name;
 	private String client_cert_name;
@@ -132,8 +130,6 @@ public class WiFiEduroam extends Activity {
 		username = (EditText) findViewById(R.id.username);
 		password = (EditText) findViewById(R.id.password);
 		
-		alertDialog = new AlertDialog.Builder(this).create();
-		
 		Button myButton = (Button) findViewById(R.id.button1);
 		if (myButton == null)
 			throw new RuntimeException("button1 not found. Odd");
@@ -150,18 +146,11 @@ public class WiFiEduroam extends Activity {
 					@Override
 					public void run() {
 						try {
-							if (csr == null) {
-								updateStatus("Generating certificate signing request... This may take a while");
-								csr = new CSR(username.getText().toString() + "@nikhef.nl");
-							}
-							
-							updateStatus("Sending CSR to the server...");
-							postData(username.getText().toString(), password.getText().toString(), csr.getCSR());
+							updateStatus("Grabbing JSON info from server...");
+							postData(username.getText().toString(), password.getText().toString());
 							
 							updateStatus("Installing WiFi profile...");
-							if (android.os.Build.VERSION.SDK_INT >= 11 && android.os.Build.VERSION.SDK_INT <= 17) {
-								installCertificates();
-							} else if (android.os.Build.VERSION.SDK_INT >= 18) {
+							if (android.os.Build.VERSION.SDK_INT >= 18) {
 								saveWifiConfig();
 								updateStatus("All done!");
 								// Clear the password field in the UI thread
@@ -251,17 +240,14 @@ public class WiFiEduroam extends Activity {
 		HashMap<String,String> configMap = new HashMap<String,String>();
 		configMap.put(INT_SUBJECT_MATCH, subject_match);
 		configMap.put(INT_ANONYMOUS_IDENTITY, "anonymous" + realm);
-		configMap.put(INT_EAP, "TLS");
-		configMap.put(INT_ENGINE, "1");
-		configMap.put(INT_ENGINE_ID, "keystore");
+		configMap.put(INT_IDENTITY, username.getText().toString());
+		configMap.put(INT_PASSWORD, password.getText().toString());
+		configMap.put(INT_EAP, "TTLS");
+		configMap.put(INT_PHASE2, "auth=PAP");
+		configMap.put(INT_ENGINE, "0");
 		configMap.put(INT_CA_CERT, INT_CA_PREFIX + ca_name);
-		configMap.put(INT_PRIVATE_KEY, INT_PRIVATE_KEY_PREFIX + client_cert_name);
-		configMap.put(INT_PRIVATE_KEY_ID, INT_PRIVATE_KEY_ID_PREFIX + client_cert_name);
-		configMap.put(INT_CLIENT_CERT, INT_CLIENT_CERT_PREFIX + client_cert_name);
 
-		if (android.os.Build.VERSION.SDK_INT >= 11 && android.os.Build.VERSION.SDK_INT <= 17) {
-			applyAndroid4_42EnterpriseSettings(currentConfig, configMap);
-		} else if (android.os.Build.VERSION.SDK_INT >= 18) {
+		if (android.os.Build.VERSION.SDK_INT >= 18) {
 			applyAndroid43EnterpriseSettings(currentConfig, configMap);
 		} else {
 			throw new RuntimeException("API version mismatch!");
@@ -283,19 +269,17 @@ public class WiFiEduroam extends Activity {
 	private void applyAndroid43EnterpriseSettings(WifiConfiguration currentConfig, HashMap<String,String> configMap) {
 		try {
 			CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-			InputStream in = new ByteArrayInputStream(Base64.decode(certificate.replaceAll("-----(BEGIN|END) CERTIFICATE-----", "")));
-			X509Certificate cert = (X509Certificate) certFactory.generateCertificate(in);
-			in = new ByteArrayInputStream(Base64.decode(ca.replaceAll("-----(BEGIN|END) CERTIFICATE-----", "")));
+			InputStream in = new ByteArrayInputStream(Base64.decode(ca.replaceAll("-----(BEGIN|END) CERTIFICATE-----", "")));
 			X509Certificate caCert = (X509Certificate) certFactory.generateCertificate(in);
 		
 			WifiEnterpriseConfig enterpriseConfig = new WifiEnterpriseConfig();
-			enterpriseConfig.setPhase2Method(Phase2.NONE);
+			enterpriseConfig.setPhase2Method(Phase2.PAP);
 			enterpriseConfig.setAnonymousIdentity(configMap.get(INT_ANONYMOUS_IDENTITY));
-			enterpriseConfig.setEapMethod(Eap.TLS);
+			enterpriseConfig.setEapMethod(Eap.TTLS);
 	
 			enterpriseConfig.setCaCertificate(caCert);
-			enterpriseConfig.setClientKeyEntry(this.csr.getPrivate(), cert);
-			enterpriseConfig.setIdentity(configMap.get(INT_ANONYMOUS_IDENTITY));
+			enterpriseConfig.setIdentity(username.getText().toString());
+			enterpriseConfig.setPassword(password.getText().toString());
 			enterpriseConfig.setSubjectMatch(configMap.get(INT_SUBJECT_MATCH));
 			currentConfig.enterpriseConfig = enterpriseConfig;
 			
@@ -304,146 +288,6 @@ public class WiFiEduroam extends Activity {
 		}
 	}
 
-	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-	// Step 1 for android 4.0 - 4.2
-	private void installCertificates() {
-		// Install the CA certificate
-		updateStatus("Inputting CA certificate.");
-		Intent intent = KeyChain.createInstallIntent();
-		intent.putExtra(KeyChain.EXTRA_NAME, ca_name);
-		intent.putExtra(KeyChain.EXTRA_CERTIFICATE, Base64.decode(ca.replaceAll("-----(BEGIN|END) CERTIFICATE-----", "")));
-		startActivityForResult(intent, 1);
-		
-	}
-
-
-	@Override
-	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-	// Step 2 for android 4.0 - 4.2; dispatcher for later steps
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		if (resultCode != RESULT_OK) {
-			updateStatus("Aborted.");
-			return;
-		}
-		if (requestCode == 1) {
-			alert("Password", "In the next dialog, type \"" + ssid + "\" as the password.");
-			return;
-		}
-		
-		if (requestCode == 2) {
-			installClientCertificate();
-			return;
-		}
-		
-		
-		if (requestCode == 3) {
-			saveWifiConfig();
-			updateStatus("All done!");
-			password.setText("");
-			return;
-		}
-		
-	}
-	
-	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-	// Step 3 for android 4.0 - 4.2
-	private void installClientCertificate() {
-		try {
-			updateStatus("Inputting client certificate.");
-			
-			// Parse the certificate that we got from the server
-			CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-			InputStream in = new ByteArrayInputStream(Base64.decode(certificate.replaceAll("-----(BEGIN|END) CERTIFICATE-----", "")));
-			X509Certificate cert = (X509Certificate) certFactory.generateCertificate(in);
-			
-			
-			client_cert_name = ssid + " " + INT_CLIENT_CERT_NAME;
-			
-			// Create a pkcs12 certificate/private key combination
-			Security.addProvider(new BouncyCastleProvider());
-			KeyStore keystore = KeyStore.getInstance("PKCS12", "BC");
-			keystore.load(null, null);
-			Certificate chain[] = new Certificate[] {(Certificate)cert}; 
-			keystore.setKeyEntry(client_cert_name, csr.getPrivate(), null, chain);
-			
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			keystore.store(out, ssid.toCharArray());
-			out.flush();
-			byte[] buffer = out.toByteArray();
-			out.close();
-			
-			// Install the private key/client certificate combination
-			Intent intent = KeyChain.createInstallIntent();
-			intent.putExtra(KeyChain.EXTRA_NAME, ssid + " " + INT_CLIENT_CERT_NAME);
-			intent.putExtra(KeyChain.EXTRA_PKCS12, buffer);
-			startActivityForResult(intent, 3);
-		} catch (CertificateException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Certificate error.");
-		} catch (KeyStoreException e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-			throw new RuntimeException("Certificate error: KeyStore");
-		} catch (NoSuchProviderException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Certificate error: Provider");
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Certificate error: Algorithm");
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Certificate error: IO");
-		}
-	}
-	
-
-	
-	
-	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-	// Last step for android 4.0 - 4.2, called from saveWifiConfig
-	private void applyAndroid4_42EnterpriseSettings(WifiConfiguration currentConfig, HashMap<String,String> configMap){
-		// NOTE: This code is mighty ugly, but reflection is the only way to get the methods we need
-		// Get the enterprise class via reflection
-		Class<?>[] wcClasses = WifiConfiguration.class.getClasses();
-		Class<?> wcEnterpriseField = null;
-
-		for (Class<?> wcClass : wcClasses) {
-			if (wcClass.getName().equals(
-					INT_ENTERPRISEFIELD_NAME)) {
-				wcEnterpriseField = wcClass;
-				break;
-			}
-		}
-		if (wcEnterpriseField == null) {
-			throw new RuntimeException("There is no enterprisefield class.");
-		}
-		
-		// Get the setValue handler via reflection
-		Method wcefSetValue = null;
-		for (Method m: wcEnterpriseField.getMethods()) {
-			if(m.getName().equals("setValue")) {
-				wcefSetValue = m;
-				break;
-			}
-		}
-		if(wcefSetValue == null) {
-			throw new RuntimeException("There is no setValue method.");
-		}
-		
-		// Fill fields from the HashMap
-		Field[] wcefFields = WifiConfiguration.class.getFields();
-		for (Field wcefField : wcefFields) {
-			if (configMap.containsKey(wcefField.getName())) {
-				try {
-					wcefSetValue.invoke(wcefField.get(currentConfig), 
-							configMap.get(wcefField.getName()));
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
@@ -466,7 +310,7 @@ public class WiFiEduroam extends Activity {
 	    	builder.setMessage(getString(R.string.ABOUT_CONTENT)+
 					"\n\n"+pi.packageName+"\n"+
 					"V"+pi.versionName+
-					"C"+pi.versionCode);
+					"C"+pi.versionCode+"-equi");
 			builder.setPositiveButton(getString(android.R.string.ok), null);
 			builder.show();
 	    	
@@ -480,7 +324,7 @@ public class WiFiEduroam extends Activity {
 	
 	
 	// This function does the HTTP POST request for provisioning and parses the JSON response
-		private void postData(String username, String password, String csr) throws RuntimeException {
+		private void postData(String username, String password) throws RuntimeException {
 		    // Create a new HttpClient and Post Header
 		    HttpClient httpclient = new DefaultHttpClient();
 		    HttpPost httppost = new HttpPost(CONF_HTTP_URL);
@@ -495,7 +339,6 @@ public class WiFiEduroam extends Activity {
 		        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
 		        nameValuePairs.add(new BasicNameValuePair("username", username));
 		        nameValuePairs.add(new BasicNameValuePair("password", password));
-		        nameValuePairs.add(new BasicNameValuePair("csr", csr));
 		        nameValuePairs.add(new BasicNameValuePair("device_id", android_id));
 		        nameValuePairs.add(new BasicNameValuePair("device_serial", android.os.Build.SERIAL));
 		        nameValuePairs.add(new BasicNameValuePair("device_description", android.os.Build.MANUFACTURER + " " + 
@@ -523,7 +366,6 @@ public class WiFiEduroam extends Activity {
 	            	throw new RuntimeException(obj.getString("error"));
 	            }
 	            // Grab the information
-	            certificate = obj.getString("certificate");
 	            ca = obj.getString("ca");
 	            ca_name = obj.getString("ca_name");
 	            realm = obj.getString("realm");
@@ -556,23 +398,6 @@ public class WiFiEduroam extends Activity {
 			};
 		});
 	}
-	
-
-	
-	private void alert(String title, String message) {
-		AlertDialog alertBox = new AlertDialog.Builder(this).create();
-		alertBox.setTitle(title);
-		alertBox.setMessage(message);
-		alertBox.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener(){
-
-			@Override
-			public void onClick(DialogInterface dialog, int which) {               
-				onActivityResult(2, RESULT_OK, null);
-			}
-		});
-		alertBox.show();
-	}
-		
 	
 	static String removeQuotes(String str) {
 		int len = str.length();
